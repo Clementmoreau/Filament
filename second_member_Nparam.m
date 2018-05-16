f6830efd3bccb7db0a45f6f1cd868973d5912587
Clementmoreau <clment.mr@gmail.com>
2018-05-16 12:26:12 +0200

    Update second_member_Nparam.m

--- second_member_Nparam.m
+++ second_member_Nparam.m
@@ -11,11 +11,6 @@ th(2)=z(3);
 
 B1=zeros(N+2,1);
 
-% for forced angular actuation
-amp=0.28*pi;
-a0p=amp*cos(t);
-a0=amp*sin(t);
-
 B1(N+2)=(th(N+1)-th(N));
 for i=N-2:-1:0
     B1(3+i)=(th(i+2)-th(i+1));
