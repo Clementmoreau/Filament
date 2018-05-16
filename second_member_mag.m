aed9027ff445a3fc9797862c6a8d45b45755cf54
Clementmoreau <clment.mr@gmail.com>
2018-05-16 12:32:17 +0200

    Update second_member_mag.m

--- second_member_mag.m
+++ second_member_mag.m
@@ -1,5 +1,11 @@
+% This function is similar to second_member, but with the magnetic effects added
+% (see Appendix VII-E, Eq. 24)
+% Hx and Hy are the external magnetic fields, defined in the MAIN file.
+
 function B=second_member_mag(t,z,N,Mag)
 global Ho Hp Hx Hy
+
+% --- Choose a magnetisation
 Mag=[zeros(1,3*N/4),ones(1,N/4)];
 th=zeros(N+1,1);
 th(2)=z(3);
@@ -10,6 +16,8 @@ B1=zeros(N+2,1);
 B2=zeros(N+2,1);
 B3=zeros(N+2,1);
 B1(N+2)=(th(N+1)-th(N));
+
+% adding the magnetic effects 
 B2(N+2)=-Mag(N)*sin(th(N));
 B3(N+2)=Mag(N)*cos(th(N));
 for i=N-2:-1:0
